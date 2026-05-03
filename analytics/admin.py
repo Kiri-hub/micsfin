78229440bd93dee8ab23b8309165da3bebf4986e
Kir <kirillbogatuk2005@gmail.com>
2026-05-04 00:06:51 +0300

    Models created, all fine

--- analytics/admin.py
+++ analytics/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+
+from .models import Student, Professor, ChessCourse
+
+admin.site.register(Student)
+admin.site.register(Professor)
+admin.site.register(ChessCourse)
--- analytics/models.py
+++ analytics/models.py
@@ -1,3 +1,50 @@
 from django.db import models
 
-# Create your models here.
+from django.db import models
+
+
+class Professor(models.Model):
+    name = models.CharField(max_length=50)
+    surname = models.CharField(max_length=50)
+    picture = models.ImageField(upload_to="professors_pics/", null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.name} {self.surname}"
+
+
+class ChessCourse(models.Model):
+    course_name = models.CharField(max_length=100)
+    course_price = models.IntegerField()
+    professor_rate = models.IntegerField()
+    per_lesson = models.BooleanField(default=False)
+    lessons_in_month = models.IntegerField(null=True, blank=True)
+
+    def __str__(self):
+        return self.course_name
+
+
+class Student(models.Model):
+    name = models.CharField(max_length=50)
+    surname = models.CharField(max_length=50)
+    is_active = models.BooleanField(default=True)
+
+    professor = models.ForeignKey(
+        Professor,
+        on_delete=models.CASCADE,
+        null=True,
+        related_name="students"
+    )
+
+    courses = models.ManyToManyField(
+        ChessCourse,
+        related_name="students"
+    )
+
+    notations = models.TextField(
+        max_length=1000,
+        default="Поки що немає нотаток",
+        blank=True
+    )
+
+    def __str__(self):
+        return f"{self.name} {self.surname}"
