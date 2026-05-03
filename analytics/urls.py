4399f3e83578c98235be33a970c7e3adfd0ed089
Kir <kirillbogatuk2005@gmail.com>
2026-05-04 01:27:32 +0300

    view_school_profit, view_professor, view_student, homepage

--- analytics/urls.py
+++ analytics/urls.py
@@ -4,4 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.homepage, name='homepage'),
+    path('', views.homepage, name="homepage"),
+    path('view-student/<int:pk>/', views.view_student, name="view_student"),
+    path('view-school-profit/', views.view_school_profit, name="view_school_profit"),
+    path('view-professor/<int:pk>/', views.view_professor, name="view_professor"),
 ]
--- analytics/views.py
+++ analytics/views.py
@@ -1,6 +1,38 @@
-from django.shortcuts import render
-from  django.http.response import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from .models import Student, Professor
 
 
 def homepage(request):
-    return HttpResponse('Helo world')
+    students = Student.objects.all()
+    return render(request, 'analytics/homepage.html', {'students': students})
+
+
+def view_student(request, pk):
+    student = get_object_or_404(Student, pk=pk)
+    return render(request, 'analytics/view_student.html', {'student': student})
+
+
+def view_professor(request, pk):
+    professor = get_object_or_404(Professor, pk=pk)
+    students = professor.professor_students.all()
+
+    return render(
+        request,
+        'analytics/view_professor.html',
+        {
+            'professor': professor,
+            'students': students
+        }
+    )
+
+
+def view_school_profit(request):
+    professors = Professor.objects.all()
+    students = Student.objects.all()
+
+    context = {
+        'professors': professors,
+        'students': students,
+    }
+
+    return render(request, 'analytics/view_school_profit.html', context)
