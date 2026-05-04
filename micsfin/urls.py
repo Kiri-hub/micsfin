0887aa006d577acdb646280ee6e87c78d3caf4ef
Kir <kirillbogatuk2005@gmail.com>
2026-05-04 03:17:54 +0300

    Proffessor picture bugfixed, media url added to setting

--- micsfin/urls.py
+++ micsfin/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
 
 
 from . import settings
@@ -25,3 +26,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('analytics.urls'))
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
