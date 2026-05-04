f37e42f87e26ce6b71e18aa783a6e07b74271185
Kir <kirillbogatuk2005@gmail.com>
2026-05-04 06:58:30 +0300

    static root added

--- micsfin/settings.py
+++ micsfin/settings.py
@@ -132,5 +132,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
